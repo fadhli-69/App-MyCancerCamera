7b4c6ce05e550a3636773b83e7331e731ff12a65
Muhammad Nurfadhli <79645149+mozart-69@users.noreply.github.com>
2024-11-03 03:33:16 +0700

    pembaruan file

--- app/src/main/java/com/dicoding/picodiploma/mycamera/data/api/FileUploadResponse.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/data/api/FileUploadResponse.kt
@@ -1,4 +1,25 @@
 package com.dicoding.picodiploma.mycamera.data.api
 
-class FileUploadResponse {
+import com.google.gson.annotations.SerializedName
+
+data class FileUploadResponse(
+    @SerializedName("message")
+    var message: String? = null,
+
+    @SerializedName("data")
+    var data: Data? = null
+) {
+
+    data class Data(
+        @SerializedName("id")
+        var id: String? = null,
+        @SerializedName("result")
+        var result: String? = null,
+        @SerializedName("confidenceScore")
+        var confidenceScore: Double? = null,
+        @SerializedName("isAboveThreshold")
+        var isAboveThreshold: Boolean? = null,
+        @SerializedName("createdAt")
+        var createdAt: String? = null
+    )
 }
